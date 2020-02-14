563fd0514c9b4080495fd64632fdc156396f69fc
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-14 13:37:39 +0300

    fin props

--- 06-anime/01-basics/javascript/main.js
+++ 06-anime/01-basics/javascript/main.js
@@ -1,5 +1,5 @@
 let targetsExample01 = anime({
-  targets: '.box',
+  targets: '.targets .box',
   translateX: 250,
   autoplay: false
 })
@@ -7,13 +7,32 @@ let targetsBtn01 = document.querySelector('#targetsBox')
 targetsBtn01.onclick = targetsExample01.play
 
 let targets02 = anime({
-  targets: '.triangle',
+  targets: '.targets .triangle',
   translateX: 150,
   autoplay: false
 })
 let targetsBtn02 =
   document.querySelector('#targetsTriangle')
 targetsBtn02.onclick = targets02.play
+
+
+// Вторая секция
+
+let propsAnimation01 = anime({
+  targets: '.props .box',
+  translateX: 400,
+  rotate: 180,
+  backgroundColor: '#fff',
+  borderRadius: ['0%', '50%'],
+  autoplay: false,
+  duration: 1000,
+  easing: 'easeInOutQuad'
+})
+let propsBtn01 = document.querySelector('#propsBtn01')
+propsBtn01.onclick = propsAnimation01.play
+
+
+
 
 
 
