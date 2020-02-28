ba61ef84b61be25f2d75fa9ab143d55487918450
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-28 11:42:30 +0300

    wip move

--- 06-anime/03-generate/index.js
+++ 06-anime/03-generate/index.js
@@ -9,5 +9,14 @@ let divsNumber = 10
 
 for (let i = 0; i < divsNumber; i+=1) {
   let div = document.createElement('div')
+  div.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
+  div.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh)`
   document.body.append(div)
 }
+
+
+
+
+
+
+//
