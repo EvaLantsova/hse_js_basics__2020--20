59fcc74426e64dd5d21205667cd79297a5e96eb7
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-28 13:04:45 +0300

    refactor function to cssText

--- 06-anime/03-generate/js/index.js
+++ 06-anime/03-generate/js/index.js
@@ -20,7 +20,8 @@ let divsNumber = 10
 for (let i = 0; i < divsNumber; i+=1) {
   let div = document.createElement('div')
 
-  div = [() => makeRect(div), () => makeCircle(div)][anime.random(0,1)]()
+  // div = [() => makeRect(div), () => makeCircle(div)][anime.random(0,1)]()
+  div.style.cssText = [makeRect(), makeCircle()][anime.random(0,1)]
 
   div.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
   div.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
@@ -33,17 +34,26 @@ for (let i = 0; i < divsNumber; i+=1) {
 }
 
 function makeRect(element) {
-  element.style.width = `${anime.random(0, 500)}px`
-  element.style.height = `${anime.random(0, 500)}px`
-  return element
+  // element.style.width = `${anime.random(0, 500)}px`
+  // element.style.height = `${anime.random(0, 500)}px`
+  // return element
+  return `
+    width: ${anime.random(0, 500)}px;
+    height: ${anime.random(0, 500)}px;
+  `
 }
 
 function makeCircle(element) {
-  let size = `${anime.random(0, 500)}px`
-  element.style.width = size
-  element.style.height = size
-  element.style.borderRadius = '100%'
-  return element
+  let size = anime.random(0, 500)
+  // element.style.width = `${size}px`
+  // element.style.height = `${size}px`
+  // element.style.borderRadius = '100%'
+  // return element
+  return `
+    width: ${size}px;
+    height: ${size}px;
+    border-radius: 100%;
+  `
 }
 
 
