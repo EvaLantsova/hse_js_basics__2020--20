b03369d5f30da270fe919dc43833f7e28b39331d
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-28 11:48:13 +0300

    wip refactor

--- 06-anime/03-generate/index.js
+++ 06-anime/03-generate/index.js
@@ -9,8 +9,12 @@ let divsNumber = 10
 
 for (let i = 0; i < divsNumber; i+=1) {
   let div = document.createElement('div')
-  div.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
-  div.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh)`
+  div.style.cssText = `
+    background: rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)});
+    transform: translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg);
+    width: ${anime.random(0, 500)}px;
+    height: ${anime.random(0, 500)}px;
+  `
   document.body.append(div)
 }
 
